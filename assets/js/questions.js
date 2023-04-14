711f8e2a3891653c747f8f4ed199b9c0046a7f64
rponce23 <rponcem75@mac.com>
2023-04-13 21:31:22 -0600

    Creating quiz questions

---
+++ assets/js/questions.js
@@ -0,0 +1,35 @@
+
+let questionsMatrix = [
+    {
+        question: "What is javascript?",
+        options: [
+            "a program language",
+            "a computer game",
+            "a text editor",
+            "none of the above"
+        ],
+        answer: 0
+    },
+    {
+        question: "Pregunra 2?",
+        options: [
+            "a program language",
+            "a computer game",
+            "a text editor",
+            "none of the above"
+        ],
+        answer: 0
+    },
+
+    {
+        question: "Pregunta 3?",
+        options: [
+            "a program language",
+            "a computer game",
+            "a text editor",
+            "none of the above"
+        ],
+        answer: 0
+    },
+
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,34 @@
-function toStart () {
-    
-}
+
+// Elements declaration
+var screenStart = document.querySelector("#start");
+var quest = document.querySelector("#quizstart");
+var startBtn = document.querySelector(".startb");
+var exitBtn = document.querySelector(".exitb");
+var outScreen = document.querySelector('.outmessage')
+
+function startfun() {
+    screenStart.classList.add('hide');
+    quest.classList.remove('hide');
+};
+
+function exitfun() {
+    screenStart.classList.add('hide');
+    outScreen.classList.remove('hide');
+};
+
+startBtn.addEventListener("click", startfun);
+exitBtn.addEventListener("click", exitfun);
+
+
+
+var h1El = document.createElement("h1");
+let pregu = questionsMatrix.map((item) => item.question);
+console.log(pregu[0]); //aqui si se ve la pregunta
+h1El.textContent = pregu[0];
+quest.append(h1El); //pero no lo muestra en pantalla
+
+
+
+
+
+
